0668d3050a1d69274ddac393f2b5b3c4cc71d5aa
Kristiyan Dobrev <christiandobrev1@gmail.com>
2022-04-30 04:23:40 +0100

    change to heroku code

--- server/index.js
+++ server/index.js
@@ -36,10 +36,13 @@ require("./routes/cart")(app);
 require("./routes/order")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
   const path = require("path");
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 }
 
